f0a2c2f59281b6818b9666ed5f0db035b6950f83
Aibek Ozhorov <ozhorov@gmail.com>
2019-09-01 23:38:34 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,13 +1,17 @@
 class  Artist 
-  attr_reader :name
+  attr_reader :name, :songs
   @@all = []
-  def initialize(name)
+  def initialize(name,songs)
     @name = name
+    @songs = songs
     @@all << self
   end
   def self.all
     @@all
   end
+  # def songs 
+  #   Artist.
+  # end
 
 
 
